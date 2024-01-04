17f6e1724534e6298fa1ab315a2d4715be04672f
Alfonsus Wiliyanto <alponklepon@gmail.com>
2024-01-04 11:25:52 +0700

    adding folder

--- src/container/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as FirstPage } from "./FirstPage";
-export { default as FirstForm } from "./FirstForm";
-export { default as SecondForm } from "./SecondForm";
-export { default as ThirdForm } from "./ThirdForm";
